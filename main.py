254c30ccbb6ce27091f5b7660f165c060dcc2514
KaelixTheFox <kaelixk@gmail.com>
2025-07-18 20:23:11 -0500

    added movement

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     print(f"Starting Asteroids!\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0, 0, 0))
+
+        player.update(dt)
+        
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        
+        # 60 fps limit
+        dt = clock.tick(60) / 1000
 if __name__ == "__main__":
     main()
