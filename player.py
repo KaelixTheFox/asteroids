92e2b8484ce0b1415e4a40ca429b18ce69d0d48c
KaelixTheFox <kaelixk@gmail.com>
2025-07-19 12:13:00 -0500

    added scoring

--- player.py
+++ player.py
@@ -5,11 +5,11 @@ from shot import Shot
 
 
 class Player(CircleShape):
-
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
         self.shoot_timer = 0
+    
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
